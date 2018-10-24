b365781bd876cee627be848973757fb39b98c25a
Nathan Talley <nathantalley@gmail.com>
2018-10-24 07:49:18 -0500

    Initial commit for INFS 3800 final PA.

---
+++ Talley_PA5/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Talley_PA5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.colorBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ibuBox = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openDatabaseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(460, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openDatabaseToolStripMenuItem
+            // 
+            this.openDatabaseToolStripMenuItem.Name = "openDatabaseToolStripMenuItem";
+            this.openDatabaseToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.openDatabaseToolStripMenuItem.Text = "Open Database";
+            this.openDatabaseToolStripMenuItem.Click += new System.EventHandler(this.openDatabaseToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(68, 59);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(289, 20);
+            this.nameBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Color:";
+            // 
+            // colorBox
+            // 
+            this.colorBox.Location = new System.Drawing.Point(68, 99);
+            this.colorBox.Name = "colorBox";
+            this.colorBox.Size = new System.Drawing.Size(100, 20);
+            this.colorBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "IBU:";
+            // 
+            // ibuBox
+            // 
+            this.ibuBox.Location = new System.Drawing.Point(257, 99);
+            this.ibuBox.Name = "ibuBox";
+            this.ibuBox.Size = new System.Drawing.Size(100, 20);
+            this.ibuBox.TabIndex = 8;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(23, 180);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(409, 220);
+            this.resultBox.TabIndex = 9;
+            this.resultBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 456);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.ibuBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.colorBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openDatabaseToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox colorBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ibuBox;
+        private System.Windows.Forms.RichTextBox resultBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Talley_PA5/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Talley_PA5
+{
+    public partial class Form1 : Form
+    {
+        // some useful variables
+        private string name;
+        private double color;
+        private double ibu;
+        private int count;
+        private double sum;
+        private string fileName;
+        private string output;
+        // helper methods
+        private double IsDouble(string x, string field)
+        {
+            if ( x != "")
+            {
+                double value;
+                if (double.TryParse(x, out value) == true)
+                {
+                    return value;
+                }
+                else
+                {
+                    MessageBox.Show(String.Format("{0} is not a valid {1}. The {1} field will be ignored for this search.", x, field));
+                    return -100;
+                }
+            }
+            else
+            {
+                return -100;
+            }
+        }
+        private bool CheckFileName()
+        {
+            if (fileName == "")
+            {
+                MessageBox.Show("Please select file first");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private bool ValidSearch()
+        {
+            if (nameBox.Text != "" || colorBox.Text != "" || ibuBox.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a search term");
+                nameBox.Focus();
+                return false;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            // set variables to default or known bad values for testing.
+            ibu = 0;
+            color = 0;
+            fileName = "";
+            count = 0;
+            sum = 0;
+            output = "";
+        }
+
+        private void openDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fileName = "";
+            // open dialog, check for valid file
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                // make sure file is correct file
+                using (var sr = new StreamReader(fileName))
+                {
+                    string line = sr.ReadLine();
+                    string[] isvalid = line.Split(',');
+                    if (isvalid.Length == 22 && isvalid[4] == "StyleID" && isvalid[8] == "ABV" && isvalid[21] == "PrimingAmount")
+                    {
+                        // assume correct file (although this is potentially flawed)
+                    }
+                    else
+                    {
+                        // tell user to select correct file
+                        MessageBox.Show("This is not the correct file. Please select the correct hombrewRecipieData file.");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // check for file selection, make sure user has entered search term
+            if (!CheckFileName() || !ValidSearch())
+            {
+                return;
+            }
+            // try and parse IBU and Color, set to -100 if unable to parse and inform user field is being ignored.
+            ibu = IsDouble(ibuBox.Text, "IBU");
+            color = IsDouble(colorBox.Text, "Color");
+            // take in some user input, make the search case insensitive
+            name = nameBox.Text.ToLower();
+            name = name.Replace(" ", "");
+            // start streamreader
+            using (var sr = new StreamReader(fileName))
+            {
+                // reset count, sum, and output accumulators
+                count = 0;
+                sum = 0;
+                output = "";
+                // main loop to cycle through each entry
+                while (!sr.EndOfStream)
+                {
+                    // read line
+                    string line = sr.ReadLine();
+                    // process line
+                    line = line.Replace("N/A", "");
+                    // split line
+                    string[] data = line.Split(',');
+                    // put into workable pieces, declare doubles to parse into
+                    string dataName = data[1];
+                    string dataURL = data[2];
+                    string dataStyle = data[3];
+                    string tempIBU = data[9];
+                    string tempColor = data[10];
+                    string tempABV = data[8];
+                    double dataIBU;
+                    double dataColor;
+                    double dataABV = 0; 
+                    // parse IBU, Color, and ABV to doubles
+                    if (double.TryParse(tempIBU, out dataIBU) == false)
+                    {
+                        dataIBU = -10000;
+                    }
+                    if (double.TryParse(tempColor, out dataColor) == false)
+                    {
+                        dataColor = -10000;
+                    }
+                    if (tempABV != "")
+                    {
+                        double.TryParse(tempABV, out dataABV);
+                    }
+                    // now search for a match
+                    // most restrictive first, must match IBU, Color, and Name or Style
+                    if (ibu != -100 && color != -100 && name != "")
+                    {
+                        if (dataName.ToLower().Contains(name) && dataColor >= color - 2 && dataColor <= color + 2 && dataIBU >= ibu - 10 && dataIBU <= ibu + 10 || dataStyle.ToLower().Contains(name) && dataColor >= color - 2 && dataColor <= color + 2 && dataIBU >= ibu - 10 && dataIBU <= ibu + 10)
+                        {
+                            // count and sum it
+                            count++;
+                            sum += dataABV;
+                            // store string output for later
+                            output += String.Format("Name: {0}\tStyle: {1}\n URL: {2}\n Color: {3}\tIBU:{4}\tABV: {5}\n\n", dataName, dataStyle, dataURL, tempColor, tempIBU, tempABV);
+                        }
+                    }
+                    // now match IBU and Name or Style
+                    else if (ibu != -100 && name != "")
+                    {
+                        if (dataName.ToLower().Contains(name) && dataIBU >= ibu - 10 && dataIBU <= ibu + 10 || dataStyle.ToLower().Contains(name) && dataIBU >= ibu - 10 && dataIBU <= ibu + 10)
+                        {
+                            // count and sum it
+                            count++;
+                            sum += dataABV;
+                            // store string output for later
+                            output += String.Format("Name: {0}\tStyle: {1}\n URL: {2}\n Color: {3}\tIBU:{4}\tABV: {5}\n\n", dataName, dataStyle, dataURL, tempColor, tempIBU, tempABV);
+                        }
+                    }
+                    // now match IBU and Color, NO name or style
+                    else if (ibu != -100 && color != -100)
+                    {
+                        if (dataColor >= color - 2 && dataColor <= color + 2 && dataIBU >= ibu - 10 && dataIBU <= ibu + 10 || dataColor >= color - 2 && dataColor <= color + 2 && dataIBU >= ibu - 10 && dataIBU <= ibu + 10)
+                        {
+                            // count and sum it
+                            count++;
+                            sum += dataABV;
+                            // store string output for later
+                            output += String.Format("Name: {0}\tStyle: {1}\n URL: {2}\n Color: {3}\tIBU:{4}\tABV: {5}\n\n", dataName, dataStyle, dataURL, tempColor, tempIBU, tempABV);
+                        }
+                    }
+                    // now match Color and Name or Style
+                    else if (color != -100 && name != "")
+                    {
+                        if (dataName.ToLower().Contains(name) && dataColor >= color - 2 && dataColor <= color + 2 || dataStyle.ToLower().Contains(name) && dataColor >= color - 2 && dataColor <= color + 2)
+                        {
+                            // count and sum it
+                            count++;
+                            sum += dataABV;
+                            // store string output for later
+                            output += String.Format("Name: {0}\tStyle: {1}\n URL: {2}\n Color: {3}\tIBU:{4}\tABV: {5}\n\n", dataName, dataStyle, dataURL, tempColor, tempIBU, tempABV);
+                        }
+                    }
+                    // match anything
+                    else if (name != "" && dataName.ToLower().Contains(name) || name != "" && dataStyle.ToLower().Contains(name) || dataColor >= color -2 && dataColor <= color +2 || dataIBU >= ibu -10 && dataIBU <= ibu +10 )
+                    {
+                        // count and sum it
+                        count++;
+                        sum += dataABV;
+                        // store string output for later
+                        output += String.Format("Name: {0}\tStyle: {1}\n URL: {2}\n Color: {3}\tIBU:{4}\tABV: {5}\n\n", dataName, dataStyle, dataURL, tempColor, tempIBU, tempABV);
+                    }
+
+                }
+            }
+            // now do output conditions if less than 50 and more than 1 result
+            if (count <= 50 && count >= 1)
+            {
+                resultBox.Text = String.Format("Found {0} results.\nAverage ABV is {1:F}\n\n", count, sum/count);
+                // past in output
+                resultBox.Text += output;
+                // clear sum, output, and count values, clear search boxes, reset focus to name
+                sum = 0;
+                count = 0;
+                output = "";
+                nameBox.Clear();
+                ibuBox.Clear();
+                colorBox.Clear();
+                nameBox.Focus();
+            }
+            // output when no result is returned
+            else if (count == 0)
+            {
+                resultBox.Text = "Found no results.";
+                // clear sum, output, and count values, clear search boxes, reset focus to name
+                sum = 0;
+                count = 0;
+                output = "";
+                nameBox.Clear();
+                ibuBox.Clear();
+                colorBox.Clear();
+                nameBox.Focus();
+            }
+            // output when more than 50 results are returned.
+            else
+            {
+                resultBox.Text = String.Format("Found {0} results. Average ABV is {1:F}\n\nToo many results. Please refine your search.", count, sum / count);
+                // clear sum, output, and count values, clear search boxes, reset focus to name
+                sum = 0;
+                count = 0;
+                output = "";
+                nameBox.Clear();
+                ibuBox.Clear();
+                colorBox.Clear();
+                nameBox.Focus();
+            }
+        }
+    }
+}
